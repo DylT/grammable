8b0f9de6d027a264fa246198f3048d4fe71163da
Dylan <dyltrz@gmail.com>
2019-10-10 22:19:18 +0000

    Added image validation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  
+  include ActionDispatch::TestProcess
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
